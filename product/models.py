52b3697dece33dad80a65313f288ade953ec35c6
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-13 02:02:55 +0330

    app.model(product): + slug field

--- product/models.py
+++ product/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
 
     # category
     name = models.CharField(max_length=50)
+    slug = models.SlugField()
     description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
