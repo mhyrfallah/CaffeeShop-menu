4af625ae8ded5f6ee6a847249ffd119c20d34b89
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-13 02:01:57 +0330

    app.model(product): + date added and date updated field

--- product/models.py
+++ product/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Product(models.Model):
 
+    # category
     name = models.CharField(max_length=50)
     description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
     
