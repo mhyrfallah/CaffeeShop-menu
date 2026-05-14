1841b853aa33c09308420b452f605b01b697b882
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-14 19:05:15 +0330

    app(product): add categories with foreignkey

--- product/models.py
+++ product/models.py
@@ -29,9 +29,9 @@ class Product(models.Model):
         (PRODUCT_UNAVAILABLE,'unavailable'),
         (PRODUCT_AVAILABLE,'available'),
     ]
-    
-    # category
 
+    # category
+    Categories = models.ForeignKey(Categories, related_name="products", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.CharField(max_length=2, default=PRODUCT_WAITING, choices=PRODUCT_STATUS)
