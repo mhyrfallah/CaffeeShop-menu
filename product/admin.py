ac692238833ebdd363cb98c6244b30722dd1a1d6
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-14 19:11:04 +0330

    (admin)add categories to DB

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Categories
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'date_added']
+
+@admin.register(Categories)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
