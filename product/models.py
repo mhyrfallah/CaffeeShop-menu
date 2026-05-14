2fdf2267e24a81a726af78e699d1e992a905b56f
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-14 17:48:08 +0330

    app(product): implement template fiel.  + update product view and model

--- product/models.py
+++ product/models.py
@@ -9,6 +9,9 @@ class Categories(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField()
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
 
@@ -19,3 +22,5 @@ class Product(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.name
--- product/views.py
+++ product/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Product, Categories
 
-class ProductMenu(generic.DetailView):
+def product_menu(request):
     model = Product
-    context_object_name = Product
-    template_name = '#'
+    context = {'product': Product.objects.all()}
+
+    return render(request, 'product.html', context)
