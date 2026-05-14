3b8d9697e67f3725257c72310385dbb51d6d1bd0
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-14 17:59:11 +0330

    config: migrate with custom user model

--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 # Custom User
-class CustomUserModel(AbstractUser):
+class CustomUserModel(AbstractBaseUser):
     pass
 
 # Product model
