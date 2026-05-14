797ca25fb87a973bf22617c04a89065289ee9489
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-14 17:24:53 +0330

    app(product): + create product view

--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product, Categories
 
-# Create your views here.
+class ProductMenu(generic.DetailView):
+    model = Product
+    context_object_name = Product
+    template_name = '#'
