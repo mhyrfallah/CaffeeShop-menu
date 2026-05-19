860f3f585ff1a6d06dc4a49847725c1ec5d331c5
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-19 11:52:46 +0330

    app(product): + image field and set settings up

--- product/models.py
+++ product/models.py
@@ -11,7 +11,7 @@ class CustomUserModel(AbstractBaseUser):
 
 class Categories(models.Model):
     name = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='category/')
+    image = models.ImageField(upload_to='category/', null=True, blank=True)
     slug = models.SlugField()
 
     def __str__(self):
