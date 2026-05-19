81fa1eaa07a488a1e8addbf227e8e6ae62a04e21
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-19 15:43:30 +0330

    update products serializer fields

--- product/serializers.py
+++ product/serializers.py
@@ -6,13 +6,14 @@ from .models import Categories, Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'slug', 'price', 'status', 'description']
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categories
         fields = '__all__'
+
 
 class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
