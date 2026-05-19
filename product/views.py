1de04abf11695a6fe33c197fadeef3b2c9cf4e81
Mahyar Fallah <Mhyr.fallah00@gmail.com>
2026-05-19 12:10:03 +0330

    config(product): + create category api view

--- product/views.py
+++ product/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Product, Categories
+from rest_framework import api_view
+from .serializers import MenuItemSerializer
+from rest_framework.response import Response
 
-def product_menu(request):
-    model = Product
-    context = {'product': Product.objects.all()}
+@api_view(['GET'])
+def menu_list(request):
+    items = Categories.objects.all()
+    serializer = MenuItemSerializer(items, many=True)
+    return Response(serializer.data)
 
-    return render(request, 'product.html', context)
+
+# def product_menu(request):
+#     model = Product
+#     context = {'product': Product.objects.all()}
+
+#     return render(request, 'product.html', context)
